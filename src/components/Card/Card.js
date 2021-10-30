c270fde817cd110c30aa45cb893f17c0579fc698
UnfriendlyKoala <p.vallejo@uniandes.edu.co>
2021-10-30 18:08:14 -0500

    Terminado pero quiero entender el cache

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -2,13 +2,11 @@ import React from 'react'
 
 export const Card = ({character}) => {
     return (
-    <div className="card" style={{width: '14rem'}}>
-        <img className="card-img-top" src={`${character.thumbnail.path}.${character.thumbnail.extension}`} alt="Card image cap"/>
-        <div className="card-body">
-            <h5 className="card-title">{character.name}</h5>
-            <p className="card-text">Description : {character.description}</p>
-            <p className="card-text">Modified : {character.modified}</p>
-        </div>
+    <div className="character" style={{border: 'solid', margin : '100px', backgroundColor: 'brown'}}>
+        <img className="char-img" src={`${character.thumbnail.path}.${character.thumbnail.extension}`} alt="Marvel Character Picture"/>
+            <h1 className="char-name">{character.name}</h1>
+            <p className="char-description"><a style={{fontWeight: 'bold'}}>Description</a> : {character.description}</p>
+            <p className="char-mod">Modified : {character.modified}</p>
     </div>
     )
 }
--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -4,8 +4,8 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 const md5 = require('md5')
 
-const PRIVATE_KEY = ''
-const PUBLIC_KEY = ''
+const PRIVATE_KEY = '6d38b9b71623b6f9a3b72d1057a9449911a20ec8'
+const PUBLIC_KEY = '39812a11e05b5d45c4723a44ca060140'
 const TS = Date.now()
 
 export const Gallery = () => {
@@ -15,14 +15,55 @@ export const Gallery = () => {
     let hash = md5(`${TS}${PRIVATE_KEY}${PUBLIC_KEY}`)
     let URL = `https://gateway.marvel.com:443/v1/public/characters?ts=${TS}&apikey=${PUBLIC_KEY}&hash=${hash}`
 
-    console.log(hash)
-    console.log(TS)
-
     useEffect(()=>{
-        fetchCharacters()
+        if(!navigator.onLine){
+            let charCache = null
+            caches.open('characters').then(cache => {
+                cache.match('/characters')
+                .then(res => {
+                    res.text().then(text => {
+                        charCache=(JSON.parse(text))
+                    })
+                })
+            })
+            
+            if(localStorage.getItem("characters") !== null) {
+                setCharacters(JSON.parse(localStorage.getItem("characters")));
+            } 
+            // else if(charCache !== null)
+            // {
+            //     //Caso para hacerlo con cache... (Por terminar)
+            // }
+            else {
+                const ch = {
+                    name : 'Loading...',
+                    description : 'Loading...',
+                    Modified: 'Loading...'
+                }
+
+                const chrs = [ch,ch,ch,ch,ch,ch]
+
+                setCharacters(chrs)
+            }
+        } else {
+            fetchCharacters()
+        }
     }, [])
 
     const fetchCharacters = () => {
+        axios.get(URL)
+        .then(res => {
+            setCharacters(res.data.data.results)
+            localStorage.setItem('characters', JSON.stringify(res.data.data.results));
+            caches.open('characters')
+            .then(cache => {
+                cache.put('characters', new Response(JSON.stringify(res.data.data.results)))
+            })
+        })
+        .catch(err => {
+            console.log(err)
+        })
+        
         axios.get(URL)
         .then(res => {
             setCharacters(res.data.data.results)
@@ -33,7 +74,7 @@ export const Gallery = () => {
     }
 
     return (
-        <div className="container-fluid" style={{display: "flex", flexWrap: "wrap", gap: '10px', justifyContent: 'center', margin: '10px 0px 10px 0px', padding: '0vw', content: '100vw'}}>
+        <div className="container-fluid">
             {characters.map((character) => (
                 <Card key = {character.id} character = {character} />
             ))}
